dcb30e8a885df9fe3ed422b7590b304da7e2805a
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-11-21 12:58:18 -0500

    Copied vagrant.py from book

---
+++ inventory/vagrant.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Adapted from Mark Mandel's implementation
+https://github.com/markmandel/vagrant_ansible_example
+"""
+from typing import List
+import argparse
+import io
+import json
+import subprocess
+import sys
+
+import paramiko
+
+
+def parse_args():
+    """command-line options"""
+    parser = argparse.ArgumentParser(description="Vagrant inventory script")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--list", action="store_true")
+    group.add_argument("--host")
+    return parser.parse_args()
+
+
+def list_running_hosts():
+    """vagrant.py --list function"""
+    cmd = ["vagrant", "status", "--machine-readable"]
+    status = subprocess.check_output(cmd).rstrip().decode()
+    hosts: List[str] = []
+    for line in status.splitlines():
+        (_, host, key, value) = line.split(",")[:4]
+        if key == "state" and value == "running":
+            hosts.append(host)
+    return hosts
+
+
+def get_host_details(host: str):
+    """vagrant.py --host <hostname> function"""
+    cmd = ["vagrant", "ssh-config", host]
+    ssh_config = subprocess.check_output(cmd).decode()
+    config = paramiko.SSHConfig()
+    config.parse(io.StringIO(ssh_config))
+    host_config = config.lookup(host)
+
+    result = {
+        "ansible_host": host_config["hostname"],
+        "ansible_port": host_config["port"],
+        "ansible_user": host_config["user"],
+        "ansible_password": "vagrant",
+    }
+    if "identityfile" in host_config:
+        result["ansible_private_key_file"] = host_config["identityfile"][0]
+
+    return result
+
+
+def main():
+    """Example 4-12 of Ansible Up & Running"""
+    args = parse_args()
+    if args.list:
+        hosts = list_running_hosts()
+        json.dump(
+            {
+                "vagrant": {"hosts": hosts, "vars": {}, "children": []},
+                "_meta": {"hostvars": {host: get_host_details(host) for host in hosts}},
+            },
+            sys.stdout,
+        )
+    else:
+        details = get_host_details(args.host)
+        json.dump(details, sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
